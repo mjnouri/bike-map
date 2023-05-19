d1506f4b228276a4a161e1a80ca50d3b1e372b8c
Mark Nouri <mjnouri@gmail.com>
2023-05-18 21:16:02 -0400

    generating rides.gpx file

--- script.py
+++ script.py
@@ -1,25 +1,32 @@
-import os, re
+import os
 
 dir = "gpx_files\\"
-# dir = "..\\runkeeper_data\\"
-
 start_pattern = "<trk>"
 end_pattern = "</trk>"
-output_file = open('output_file.txt', 'a')
+# create rides.gpx if it doesn't exist, clear contents if it does
+open('rides.gpx', 'w').close()
+output_file = open('rides.gpx', 'a')
+first_file = True
 
 for file in os.listdir(dir):
 	if file.endswith(".gpx"):
-		file_path = dir + file
-		opened_file = open(file_path, "r")
-		lines = opened_file.readlines()
-		for count, value in enumerate(lines):
-			if start_pattern in value:
-				start_line_number = count + 1
-			if end_pattern in value:
-				end_line_number = count + 1
-		print(f"File: {file_path}\nStart line: {start_line_number}\nEnd line: {end_line_number}")
-		# output_file.write(opened_file[opened_file.index(start_line_number):opened_file.index(end_line_number)])
-		# output_file.write(str(opened_file))
-		print(opened_file)
-		opened_file.close()
-		print()
+		current_file_path = dir + file
+		current_file = open(current_file_path, "r")
+		lines = current_file.readlines()
+		if (first_file):
+			for i in lines[:8]:
+				output_file.write(i)
+			output_file.write("\n\n")
+		first_file = False
+		for index, line_value in enumerate(lines):
+			if start_pattern in line_value:
+				start_line_number = index
+			if end_pattern in line_value:
+				end_line_number = index + 1
+		print(f"File: {current_file_path}\nStart line: {start_line_number}\nEnd line: {end_line_number}\n")
+		for i in lines[start_line_number:end_line_number]:
+			output_file.write(i)
+		output_file.write("\n\n")
+
+output_file.write("</gpx>")
+output_file.close()

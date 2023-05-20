4cbf5ef0f2c0f361ab9addf6ba25e3ae81dc42d9
Mark Nouri <mjnouri@gmail.com>
2023-05-20 16:37:43 -0400

    added geolocation detection to known trails

--- script.py
+++ script.py
@@ -1,4 +1,7 @@
 import os
+import haversine as hs
+from haversine import Unit
+import gpxpy
 
 dir = "gpx_files\\"
 start_pattern = "<trk>"
@@ -9,34 +12,54 @@ open('rides.gpx', 'w').close()
 output_file = open('rides.gpx', 'a')
 first_file = True
 
+# dictionary of starting coordinates at each trailhead where keys = str, values = tuples, each tuple has 2 floats
+trailhead_coordinates = {'Haverstraw Trail':  (41.182578, -73.951956),
+        				 'Paulinskill Trail': (41.085281, -74.699790),
+        				 'Columbia Trail':    (40.817641, -74.724419)}
+
 #  perform the following actions for each .gpx file in gpx_files\
 for file in os.listdir(dir):
 	if file.endswith(".gpx"):
 		current_file_path = dir + file
 		current_file = open(current_file_path, "r")
 		lines = current_file.readlines()
+
 		# when building rides.gpx, add the opening elements from the first file
 		if (first_file):
 			for i in lines[:8]:
 				output_file.write(i)
 			output_file.write("\n\n")
 		first_file = False
+
 		# find the line numbers where <trk> and </trk> are in each .gpx file
 		for index, line_value in enumerate(lines):
 			if start_pattern in line_value:
 				start_line_number = index
 			if end_pattern in line_value:
 				end_line_number = index + 1
-		print(f"File: {current_file_path}\nStart line: {start_line_number}\nEnd line: {end_line_number}\n")
+		# print(f"File: {current_file_path}\nStart line: {start_line_number}\nEnd line: {end_line_number}\n")
+
 		# add the contents between <trk> and </trk> from each file to rides.gpx
 		for i in lines[start_line_number:end_line_number]:
 			output_file.write(i)
 		output_file.write("\n\n")
-		# next step - geolocate first lat/lon point of each .gpx file to get name of trail
-			# in a map variable, store each trail name and starting point coordinate
-			# store first coordinate in .gpx file to variable
-			# if variable coordinate is within 1000' from any starting point in the map variable
-			# output matching trail name from map to csv file column
+		current_file.close()
+
+		# geolocate starting point in gpx file and match it up to which trail it is
+		gpx_file = open(current_file_path, "r")
+		gpx_data = gpxpy.parse(gpx_file)
+		latitude_starting_point = gpx_data.tracks[0].segments[0].points[0].latitude
+		longitude_starting_point = gpx_data.tracks[0].segments[0].points[0].longitude
+		file_starting_coordinate = latitude_starting_point,longitude_starting_point
+
+		# loop through trailhead coordinates and look for where the current gpx file starting point is closest to
+		for trail_name, trail_coordinates in trailhead_coordinates.items():
+			difference_in_distance = hs.haversine(file_starting_coordinate,trail_coordinates,unit=Unit.FEET)
+			if difference_in_distance < 500:
+				print("Trail found in gpx file is " + trail_name)
+				print(f"First gpx coordinate is " + str(int(difference_in_distance)) + " feet away from " + trail_name + " trailhead.")
+				print()
+		gpx_file.close()
 
 # add the closing element to rides.gpx
 output_file.write("</gpx>")

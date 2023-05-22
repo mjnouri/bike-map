5b80ffbc369b6a20d2843897e23d58fe20d388f6
Mark Nouri <mjnouri@gmail.com>
2023-05-21 22:08:20 -0400

    get distance traveled from files

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import os
 import haversine as hs
 from haversine import Unit
 import gpxpy
+import subprocess
 
 dir = "gpx_files\\"
 start_pattern = "<trk>"
@@ -15,11 +16,11 @@ ride_count = 0
 
 # dictionary of starting coordinates at each trailhead where keys = str, values = tuples, each tuple has 2 floats
 trailhead_coordinates = {
+			'Lincoln Park Trail': (40.919203, -74.300907),
 			'Haverstraw Trail':  (41.182578, -73.951956),
 			'Columbia Trail':    (40.817641, -74.724419),
 			'Paulinskill Trail': (41.085281, -74.699790),
 			'Paulinskill Trail2': (40.992136, -74.910228),
-			'Lincoln Park Trail': (40.919203, -74.300907),
 			'Highlands Trail': (40.401271, -73.984361),
 			'Cedar Grove Trail': (40.858259, -74.226300),
 			'Battlefield Trail': (40.523669, -74.492918),
@@ -32,7 +33,7 @@ trailhead_coordinates = {
 			'Sterling Forest Trail': (41.206681, -74.239067),
 			'Cannonball Trail': (41.047944, -74.251842)}
 
-#  perform the following actions for each .gpx file in gpx_files\
+# perform the following actions for each .gpx file in gpx_files\
 for file in os.listdir(dir):
 	if file.endswith(".gpx"):
 		current_file_path = dir + file
@@ -52,7 +53,7 @@ for file in os.listdir(dir):
 				start_line_number = index
 			if end_pattern in line_value:
 				end_line_number = index + 1
-		# print(f"File: {current_file_path}\nStart line: {start_line_number}\nEnd line: {end_line_number}\n")
+		print("File: {0}\nStart line: {1}\nEnd line: {2}".format(current_file_path, start_line_number, end_line_number))
 
 		# add the contents between <trk> and </trk> from each file to rides.gpx
 		for i in lines[start_line_number:end_line_number]:
@@ -60,22 +61,32 @@ for file in os.listdir(dir):
 		output_file.write("\n\n")
 		current_file.close()
 
-		# geolocate starting point in gpx file and match it up to which trail it is
+		# get first coordinate from gpx file
 		gpx_file = open(current_file_path, "r")
 		gpx_data = gpxpy.parse(gpx_file)
 		latitude_starting_point = gpx_data.tracks[0].segments[0].points[0].latitude
 		longitude_starting_point = gpx_data.tracks[0].segments[0].points[0].longitude
 		file_starting_coordinate = latitude_starting_point,longitude_starting_point
 
-		# loop through trailhead coordinates and look for where the current gpx file starting point is closest to
+		# check distance between first coordinate in gpx file and each trailhead coordinate, return closest trailhead to starting point
 		for trail_name, trail_coordinates in trailhead_coordinates.items():
 			difference_in_distance = hs.haversine(file_starting_coordinate,trail_coordinates,unit=Unit.FEET)
 			if difference_in_distance < 500:
-				print("Trail found in gpx file is " + trail_name + ".")
-				print(f"First gpx coordinate is " + str(int(difference_in_distance)) + " feet away from " + trail_name + " trailhead.")
+				print("Trail found in .gpx file is " + trail_name + ".")
+				# print("Trail found in gpx file is {0}."format(trail_name))
+				print("First gpx coordinate is " + str(int(difference_in_distance)) + " feet away from " + trail_name + " trailhead.")
+				# print("First gpx coordinate is {0} feet away from {1} trailhead."format(difference_in_distance, trail_name))
 				ride_count += 1
-				print()
+				print("Ride count: " + str(ride_count))
 		gpx_file.close()
+
+		# get distance traveled in miles from gpx file using gpx-cmd-tools
+		cmd = "python ..\gpx-cmd-tools\gpxinfo {0} --track --miles".format(current_file_path)
+		cmd_output = subprocess.check_output(cmd)
+		decoded_output = cmd_output.decode("utf-8")
+		print(decoded_output)
+
+		# edit gpx file <name> to be this format: <name><![CDATA[Lincoln Park Trail - 21.92 miles - Cycling 2-26-21]]></name>
 
 # add the closing element to rides.gpx
 output_file.write("</gpx>")
